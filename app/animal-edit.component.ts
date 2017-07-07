6156399b0e85f22f183ef98541589b8c678295a3
eluts <eluts15@gmail.com>
2017-07-07 12:05:01 -0700

    begin building new EditAnimalInfo component.

---
+++ app/animal-edit.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { AnimalInfo } from  './animal_info.model';
+
+@Component ({
+  selector: 'edit-animal-info',
+  template: `
+
+  `
+})
+
+
+export class EditAnimalInfoComponent {
+  
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { AnimalListComponent } from './animal-list.component';
 import { NewAnimalComponent } from './new-animal.component';
+import { EditAnimalInfoComponent } from './animal-edit.component';
 
 @NgModule ({
   imports: [ BrowserModule, FormsModule ],
   declarations: [ AppComponent,
                   AnimalListComponent,
                   NewAnimalComponent,
+                  EditAnimalInfoComponent,
   ],
 
   bootstrap: [ AppComponent ]
