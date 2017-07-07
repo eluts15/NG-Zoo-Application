261051349151e4f54ff7df1b1f3409b02236973b
eluts <eluts15@gmail.com>
2017-07-07 09:34:36 -0700

    add angular elements in app.component for future use. Create a basic list of animal info that will be shown on the screen, install npm components.

---
+++ app/animal-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AnimalInfo } from './animal_info.model';
+
+@Component({
+  selector: 'animal-list',
+  template: `
+
+  `
+})
+
+export class AnimalListComponent {
+
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -6,9 +6,22 @@ import { AnimalInfo } from './animal_info.model.model';
   template: `
   <div class="container">
     <h3>{{header}}</h3>
+    <animal-list></animal-list>
+    <edit-animal></edit-animal>
+    <add-animal></add-animal>
   `
 })
 
 export class AppComponent {
   header: string = 'Hello World!';
+  selectedAnimal = null;
+
+  masterAnimalList: AnimalInfo[] = [
+    new AnimalInfo('Arctic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud noises'),
+    new AnimalInfo('Northwest Black Tailed Bear', 'Tinkerbell', 6, 'Herbivore', 'Northern Trail', 3, 'Female', 'Delicate chocolate  and leaves', 'Foxes'),
+  ];
+
+  addAnimal (newAnimalFromChild: AnimalInfo) {
+    this.masterAnimalList.push(newAnimalFromChild);
+  }
 }
